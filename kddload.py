8c3298fd3a20f457372da4975686090e050e3576
Marc Schoolderman <mschool@science.ru.nl>
2013-05-14 19:49:59 +0200

    removed limit

--- kddload.py
+++ kddload.py
@@ -77,9 +77,7 @@ def read_csv(table, force_creation=False):
 read_csv("Conference")
 read_csv("Journal")
 read_csv("Author")
-limit = 100000
 read_csv("Paper")
-limit = 10000
 read_csv("PaperAuthor")
 
 db['DeletedPaper'] = db['Paper']
