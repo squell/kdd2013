7970188e5536cfa9f225bb48b6c0a12f01323576
Marc Schoolderman <mschool@science.ru.nl>
2013-05-26 23:24:48 +0200

    feature cutter

---
+++ cut_feat.py
@@ -0,0 +1,31 @@
+'''
+    reduce a feature-pickle file by selecting features
+'''
+
+from kddutil import *
+import cPickle as pickle
+import sys
+
+if len(sys.argv) <= 3:
+    print "pickel-in pickel-out selection [selection....]"
+    sys.exit()
+
+print "reading"
+with open(sys.argv[1],"rb") as infile:
+    train, test = pickle.load(infile)
+
+selected = map(int, sys.argv[3:])
+print "selected: ", selected
+
+ids, features, labels = train
+features = map(lambda feat: [row for (i,row) in enumerate(feat) if i in selected], features)
+train = ids, features, labels
+
+ids, features = test
+features = map(lambda feat: [row for (i,row) in enumerate(feat) if i in selected], features)
+test = ids, features
+
+print "writing"
+with open(sys.argv[2],"wb") as outfile:
+    pickle.dump((train,test), outfile)
+
