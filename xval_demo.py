dbf594a57a2da1796c7532b51193a8d7cfdb6b63
Marc Schoolderman <mschool@science.ru.nl>
2013-05-28 23:50:53 +0200

    moved disambiguation into evaluate(); since the kdd cup is weird

--- xval_demo.py
+++ xval_demo.py
@@ -12,7 +12,14 @@ if len(sys.argv) <= 1:
     print "so what pkl do you want me to read, hm?"
     sys.exit()
 
-randomForest = ensemble.RandomForestClassifier(verbose=True
+randomForest = ensemble.RandomForestClassifier(verbose=False
+	, n_estimators=80
+	, min_samples_split=10
+	, max_depth=14
+	, n_jobs=15
+	)
+
+randomForestRegress = ensemble.RandomForestClassifier(verbose=True
 	, n_estimators=80
 	, min_samples_split=10
 	, max_depth=14
@@ -29,13 +36,12 @@ with open(sys.argv[1]) as infile:
     train, _ = pickle.load(infile)
 
 # filter out entries which are marked both as positive AND negative
-ids, info, labels = kddutil.disambiguate(*train)
-
+ids, info, labels = train
 info = kddutil.bound(info, max=10000, min=-10000)
 
 print "Random Forest"
-print kddutil.evaluate_k_(randomForest, ids, info, labels)
+print kddutil.evaluate_k(randomForest, ids, info, labels)
 
-print "Gradient Boosting"
-print kddutil.evaluate(gradBoost, ids, info, labels)
+#print "Gradient Boosting"
+#print kddutil.evaluate(gradBoost, ids, info, labels)
 
