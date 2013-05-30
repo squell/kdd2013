b58c5868c6a09b3a09eca4c5c899c13362e2c983
Marc Schoolderman <mschool@science.ru.nl>
2013-05-30 16:06:48 +0200

    added lcsubsequence, lcsubstring

--- scrabble.py
+++ scrabble.py
@@ -47,3 +47,36 @@ def ed_bi(word1,word2):
             D[i] = (nlw,c2)
     return D[-1][0]
 
+def ned(word1,word2):
+    'approximate-normalized edit distance'
+    try:
+        return ed(word1,word2)/float(max(len(word1),len(word2)))
+    except ZeroDivisionError:
+	return 0.0
+
+def lcs(word1,word2):
+    'longest common subsequence'
+    D = [(0,c) for c in [None]+list(word2)]
+    for c in word1:
+        olw = nlw = -float('inf')
+        for i, pair in enumerate(D):
+            w, c2 = pair
+            olw, w = w, max(w, nlw, olw+1 if c==c2 else w)
+            nlw = w
+            D[i] = (nlw,c2)
+    return D[-1][0]
+
+def lcstr(word1,word2):
+    'longest common substring'
+    D = [(0,c) for c in [None]+list(word2)]
+    M = (0,None)
+    for c in word1:
+        olw = nlw = -float('inf')
+        for i, pair in enumerate(D):
+            w, c2 = pair
+            olw, w = w, (olw+1 if c==c2 else 0)
+            nlw = w
+            D[i] = (nlw,c2)
+	M = max([M]+D, key=lambda x:x[0])
+    return M[0]
+
